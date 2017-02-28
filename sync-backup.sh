180f9141dc3bdc71597a4fc27b335bad586657a6
vixfree <stvixfree@gmail.com>
2017-02-28 03:49:14 +0300

    create sync-backup.sh
    The script is copying files from the given extensions .. (weak version of the processor)

---
+++ sync-backup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+## athor Koshuba V.O. - master@qbpro.ru
+## The script is copying files from the given extensions .. (weak version of the processor)
+#date
+rdate=`date +%d.%m.%y_%H:%M`
+#logfile
+log=/var/log/syslog
+
+#the master directory on the server
+smbuser=( user1 user2 user3 user4 );
+smbgroup=( group1 group2 group3 group4 )
+## allowed extensions
+tru_file=( "bmp"
+	   "cdw"
+	   "doc"
+	   "docx"
+	   "dwg"
+	   "dwl"
+	   "dwl2"
+	   "dxf"
+	   "frw"
+	   "jpg"
+	   "ldb"
+	   "mdb"
+	   "odb"
+	   "odt"
+	   "pdf"
+	   "pfl"
+	   "psd"
+	   "PST"
+	   "QST"
+	   "shx"
+	   "tif"
+	   "ttf"
+	   "txt"
+	   "upd"
+	   "xls"
+	   "xlsx"
+	   "xml"
+	   "rtf"
+	   "ods" );
+# synchronize directories
+get_dir=( "/work/share"
+	 "/work/share"
+	 "/share2"
+	 "/share2" );
+# directly copied directories
+read_dir=( "work1"
+	 "work2"
+	 "work3"
+	 "work4" );
+# directories to backup files
+put_dir=( "/arhive/copy_share"
+	 "/arhive/copy_share"
+	 "/arhive/copy_share"
+	 "/arhive/copy_share" );
+#------------------------------------------------
+
+# Copy the specified directory on the server and update Regulations
+# We will perform the cycle until the end of the array of names
+for ((i_names=0; i_names != ${#get_dir[@]}; i_names++))
+    do
+    cd ${get_dir[$i_names]};
+# We will perform the cycle until the end of the array of allowed extensions
+    for ((i_ex=0; i_ex != ${#tru_file[@]}; i_ex++))
+	do
+	find ${read_dir[$i_names]} -type 'f' -name "*.${tru_file[$i_ex]}" -exec cp -r -f --parent -t ${put_dir[$i_names]} '{}' \;
+    done
+# Assign user permissions
+chmod -R 0666 ${put_dir[$i_names]}/${read_dir[$i_names]};
+chmod -R ugo+X ${put_dir[$i_names]}/${read_dir[$i_names]};
+chown -R ${smbuser[$i_names]}:${smbgroup[$i_names]} ${put_dir[$i_names]}/${read_dir[$i_names]};
+done
+
+# 
+echo `date +%d.%m.%y_%H:%M` "Leaving syncing files on the server...">>$log
+exit 0
